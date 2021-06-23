b1e73d1816c555e2f3fdbc76b366100593c9ad76
automatonduck <Sunflower7!>
2021-06-23 19:48:36 -0400

    added missing diag win check

--- tiktactoe.py
+++ tiktactoe.py
@@ -73,20 +73,26 @@ def check_win():
     for row in range(0,3):
         if ((TTT [row][0] == TTT[row][1] == TTT[row][2]) and (TTT[row][0] is not None)):
             winner = TTT[row][0]
-            pg.draw.line(screen, (250,0,0), ((col+1)*width/3-width/6,0),((col+1)*width/3-width/6, height),4)
+            pg.draw.line(screen, (250,0,0), (0, (row + 1)*height/3 -height/6),\
+                              (width, (row + 1)*height/3 - height/6 ), 4)
             break
     #check columns
     for col in range(0,3):
         if ((TTT [0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None)):
-            winner = TTT[0][col]
-            pg.draw.line(screen, (250,0,0), ((col+1)*width/3-width/6,0),((col+1)*width/3-width/6, height),4)
+            pg.draw.line (screen, (250,0,0),((col + 1)* width/3 - width/6, 0),\
+                          ((col + 1)* width/3 - width/6, height), 4)
             break
     #check diagnols
     if (TTT[0][0] == TTT[1][1] == TTT[2][2]) and (TTT[0][0] is not None):
         winner = TTT[0][0]
         pg.draw.line(screen, (250,70,70), (350,50), (50,350),4)
+    if (TTT[0][2] == TTT[1][1] == TTT[2][0]) and (TTT[0][2] is not None):
+            # game won diagonally right to left
+        winner = TTT[0][2]
+        pg.draw.line (screen, (250,70,70), (350, 50), (50, 350), 4)
     if(all([all(row) for row in TTT]) and winner is None):
-        draw =True
+        draw = True
+        
     draw_status()
 
 def drawXO(row,col):
