f4c1030ae218068bdf5130df01567f764635168c
automatonduck <Sunflower7!>
2021-06-23 19:09:47 -0400

    added check_win function

--- tiktactoe.py
+++ tiktactoe.py
@@ -65,3 +65,27 @@ def draw_status():
     text_rect = text.get_rect(center=(width/2, 500-50))
     screen.blit(text, text_rect)
     pg.display.update()
+
+def check_win():
+    global TTT, winner,draw
+    
+    #check for rows
+    for row in range(0,3):
+        if ((TTT [row][0] == TTT[row][1] == TTT[row][2]) and (TTT[row][0] is not None)):
+            winner = TTT[row][0]
+            pg.draw.line(screen, (250,0,0), ((col+1)*width/3-width/6,0),((col+1)*width/3-width/6, height),4)
+            break
+    #check columns
+    for col in range(0,3):
+        if ((TTT [0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None)):
+            winner = TTT[0][col]
+            pg.draw.line(screen, (250,0,0), ((col+1)*width/3-width/6,0),((col+1)*width/3-width/6, height),4)
+            break
+    #check diagnols
+    if (TTT[0][0] == TTT[1][1] == TTT[2][2]) and (TTT[0][0] is not None):
+        winner = TTT[0][0]
+        pg.draw.line(screen, (250,70,70), (350,50), (50,350),4)
+    if(all([all(row) for row in TTT]) and winner is None):
+        draw =True
+    draw_status()
+
